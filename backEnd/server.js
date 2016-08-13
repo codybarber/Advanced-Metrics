d9a8d65e587c32f91dd44c76f766269da2e603af
Cody Barber <cody.n.barber@gmail.com>
2016-08-13 01:57:19 -0400

    Building out sub pages for angular routing

--- backEnd/server.js
+++ backEnd/server.js
@@ -8,6 +8,24 @@ var cors = require('cors');
 var db = pgp('postgres://localhost:5432/baseball_db');
 
 
-app.listen(3000, function() {
-  console.log('listening on port 3000');
+// db.one('insert into teams(team_name, level) values($1, $2) returning id', ['Yankees', 1])
+//   .then(function(data) {
+//     console.log(data);
+//   })
+//   .then(function(error) {
+//     console.log(error);
+//   });
+
+app.post('/get-stats', function(request, response) {
+  db.any('select team_name as name from teams')
+    .then(function(data) {
+      response.send(data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+app.listen(8000, function() {
+  console.log('listening on port 8000');
 });
--- frontEnd/js/app.js
+++ frontEnd/js/app.js
@@ -1,23 +1,92 @@
+
 var app = angular.module('statApp', ['ngRoute', 'ngCookies']);
 
 app.config(function($routeProvider) {
   $routeProvider
 
   .when('/', {
-    templateUrl: '/static/home.html',
+    templateUrl: '/home.html',
     controller: 'MainController'
   })
   .when('/signup', {
-    templateUrl: '/static/signup.html',
+    templateUrl: '/signup.html',
     controller: 'SignupController'
   })
   .when('/login', {
-    templateUrl: '/static/login.html',
+    templateUrl: '/login.html',
     controller: 'LoginController'
+  })
+  .when('/add-team', {
+    templateUrl: '/add-team.html',
+    controller: 'AddTeamController'
+  })
+  .when('/add-player', {
+    templateUrl: '/add-player.html',
+    controller: 'AddPlayerController'
+  })
+  .when('/add-game-stats', {
+    templateUrl: '/add-game-stats.html',
+    controller: 'MainController'
+  })
+  .when('/stat-entry', {
+    templateUrl: '/stat-entry.html',
+    controller: 'StatEntryController'
+  })
+  .when('/stat-display', {
+    templateUrl: '/stat-display.html',
+    controller: 'StatDisplayController'
   });
 });
 
-var API = 'http://localhost:3000';
+var API = 'http://localhost:8000';
+
+app.factory('batting', function($http) {
+  var hitting = {};
+  hitting.battingAverage = function(atBats, hits) {
+    var ba = hits / atBats;
+    return ba;
+  }
+  hitting.walkPercentage = function(walks, plateAppearances) {
+    var wp = walks / plateAppearances;
+    return wp;
+  }
+  hitting.strikeoutPercentage = function(strikeouts, plateAppearances) {
+    var sop = strikeouts / plateAppearances;
+    return sop;
+  }
+  hitting.sluggingPercentage = function(totalBases, atBats) {
+    var slg = totalBases / atBats;
+    return slg;
+  }
+  hitting.isolatedPower = function(sluggingPercentage, battingAverage) {
+    var iso = sluggingPercentage - battingAverage;
+    return iso;
+  }
+  hitting.babip = function(hits, homeruns, atBats, strikeouts, sacrificeFlys) {
+    var babip = (hits - homeruns) / (atBats - strikeouts - homeruns + sacrificeFlys);
+    return babip;
+  }
+  hitting.onBasePercentage = function(hits, walks, hitByPitch, atBats, sacrificeFlys) {
+    var obp = (hits + walks + hitByPitch) / (atBats + walks + hitByPitch + sacrificeFlys);
+    return obp;
+  }
+  hitting.runsCreated = function(hits, walks, totalBases, atBats) {
+    var rc = ((hits + walks) * totalBases) / (atBats + walks);
+    return rc;
+  }
+  hitting.onBasePlusSlugging = function(onBasePercentage, sluggingPercentage) {
+    var ops = onBasePercentage + sluggingPercentage;
+    return ops;
+  }
+  hitting.weightedOnBaseAverage = function(walks, hitByPitch, singles, doubles, triples, homeruns, atBats, intentionalWalks, sacrificeFlys) {
+    var woba = ((0.689 * walks) + (0.720 * hitByPitch) + (0.878 * singles) + (1.244 + doubles) + (1.573 + triples) + (2.024 + homeruns)) / (atBats + walks - intentionalWalks + sacrificeFlys + hitByPitch);
+    return woba;
+  }
+  hitting.weightedRunsAboveAverage = function(weightedOnBaseAverage) {
+    var wraa = (weightedOnBaseAverage - 0.317) / 1.25;
+    return wraa;
+  }
+});
 
 app.controller('MainController', function($scope, $http, $location) {
 
@@ -30,3 +99,31 @@ app.controller('SignupController', function($scope, $http, $location) {
 app.controller('LoginController', function($scope, $http, $location) {
 
 });
+
+app.controller('AddTeamController', function($scope, $http, $location) {
+
+});
+
+app.controller('AddPlayerController', function($scope, $http, $location) {
+
+});
+
+app.controller('StatEntryController', function($scope, $http, $location) {
+
+});
+
+app.controller('StatDisplayController', function($scope, $http, $location) {
+  $http.post(API + '/get-stats')
+  .success(function(data) {
+    $scope.stats = data;
+    console.log($scope.stats);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+});
+
+// app.run(function($rootScope, $location, $cookies) {
+//
+// });
